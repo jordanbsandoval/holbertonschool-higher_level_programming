03fd199a1157adf0bdfa951ef58cfb2996024a5e
jordan buitrago <1608@holbertonschool.com>
2021-03-11 12:32:18 -0500

    hot-fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,4 @@
 -- script that creates a table second_table
 -- in database hbtn_0c_0
--- id INT
--- name VARCHAR(256)
--- score INT
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 18);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
