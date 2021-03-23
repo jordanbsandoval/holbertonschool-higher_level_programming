4561f6ecb70643a0a0d5e590c70428c9474522fa
jordanbsandoval <1608@holbertonschool.com>
2021-03-23 18:38:42 -0500

    hotfix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['c is fun', 'Python is cool', 'JavaScript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 console.log(array[0]);
 console.log(array[1]);
 console.log(array[2]);
