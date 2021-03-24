b1d2e9ab4b84765107f7f2ffdcd152dbb3a20d91
jordanbsandoval <1608@holbertonschool.com>
2021-03-23 20:18:20 -0500

    hotfix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const descriptor = process.argv[2];
-if (descriptor !== undefined)
-{
-    console.log(descriptor);
-}else
-{
-    console.log("No argument");
+if (descriptor !== undefined) {
+  console.log(descriptor);
+} else {
+  console.log('No argument');
 }
