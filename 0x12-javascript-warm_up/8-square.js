80f770cfc82256dbf0ac308f9f8c0343a79444f1
jordanbsandoval <1608@holbertonschool.com>
2021-03-23 22:48:13 -0500

    not knowing

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 // Script that print an square
-const number = process.argv[2];
-if (isNaN(number)) 
-{
+const number = parseInt(process.argv[2]);
+if (isNaN(number)) {
   console.log('Missing size');
-} else 
-{
-  for (let i = 0; i < number; i++) 
-  {
+} else {
+  for (let i = 0; i < number; i++) {
     console.log('x'.repeat(number));
   }
 }
