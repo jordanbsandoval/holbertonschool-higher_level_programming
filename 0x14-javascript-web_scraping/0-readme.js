05233e40d036e4b94770d9e36dba7c72450822a3
jordanbsandoval <1608@holbertonschool.com>
2021-05-05 00:49:08 -0400

    try4

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 'use strict';
-let fs = require("fs");
-let dato = process.argv[2];
+const fs = require("fs");
+const dato = process.argv[2];
 
-fs.readFile(dato, 'utf8', function (err, data){
+fs.readFile(dato, 'utf8', function (err, data) {
   if (err) {
     console.error(err);
     return;
