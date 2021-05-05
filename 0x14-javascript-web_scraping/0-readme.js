07b414d83dc440cffc3015d9afdd58be8ce25e72
jordanbsandoval <1608@holbertonschool.com>
2021-05-05 00:48:12 -0400

    try3

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
 'use strict';
-// importando el modulo fs
 let fs = require("fs");
-
-// obteniendo el path del archivo
 let dato = process.argv[2];
 
-
-fs.readFile(dato, 'utf8', function (err, data)
-{
-    if (err) 
-    {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+fs.readFile(dato, 'utf8', function (err, data){
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
