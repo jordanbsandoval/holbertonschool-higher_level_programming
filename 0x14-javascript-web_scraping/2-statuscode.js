ef559158b4bc591478a0a18ee40928595320f8b3
jordanbsandoval <1608@holbertonschool.com>
2021-05-05 18:58:52 -0400

    status_code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -8,6 +8,6 @@ const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    console.log('Code:', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
