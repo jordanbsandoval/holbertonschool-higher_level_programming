f18f9d6679168cf49edfd91b97c9bc3a014ac858
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 11:38:05 -0400

    parseInt

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 'use strict';
 // write an script that print a square
-const entrada = process.argv[2];
-if (!entrada) {
-  console.log('Missing size');
-} else if (isNaN(entrada)) {
+const entrada = parseInt(process.argv[2]);
+if (isNaN(entrada)) {
   console.log('Missing size');
 } else if (entrada > 0) {
   for (let i = 0; i < entrada; i++) {
-    console.log('x'.repeat(entrada));
+    console.log('X'.repeat(entrada));
   }
 }
