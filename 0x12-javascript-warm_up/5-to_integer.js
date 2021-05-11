d9fbc160d1b3aa5b74632c706f5121b87d486369
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 11:58:18 -0400

    add 5-to_integer.js(

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // Write a script that prints My number: ... if the first argument can be converted to an integer
 // If the argument can’t be converted to an integer, print “Not a number”
 const number = parseInt(process.argv[2]);
-if (isNaN(number)) {
+if (!(number)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + number);
