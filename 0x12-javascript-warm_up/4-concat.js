0927c6ed85c09ba3460157ebef6e806f3dd6e825
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 01:14:59 -0400

    process.argv

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-console.log(arg1 + ' is ' + arg2);
+//Write a script that prints two arguments passed to it, in the following format: " is "
+const word = ' is ';
+const entrada = process.argv[2];
+const entrada2 = process.argv[3];
+
+if (entrada && entrada2) {
+  console.log(entrada + word + entrada2);
+} else if (entrada) {
+  console.log(entrada + word + undefined);
+} else {
+  console.log(undefined + word + undefined);
+}
