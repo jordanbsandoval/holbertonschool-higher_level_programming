80d410e06681854b1c7d1763b853576e24b5f4e3
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 00:47:57 -0400

    console.log

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(array[0]);
-console.log(array[1]);
-console.log(array[2]);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
