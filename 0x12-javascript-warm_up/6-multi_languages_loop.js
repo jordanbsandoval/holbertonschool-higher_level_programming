6bbd3da4e6794f9e69852c8845ee3119d18a608e
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 01:29:17 -0400

    forEach

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const i in array) {
-  console.log(array[i]);
-}
+// Write a script that prints 3 lines:  but by using an array of string and a loop
+const myArray = ['c is fun', 'Python is cool', 'JavaScript is amazing'];
+myArray.forEach(v => { console.log(v); });
