6dc65885ce08d29fbde5c8c91e02c625bfa6ef00
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 13:20:31 -0400

    sort().slice()

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
-const value = process.argv;
-if (value.length <= 3) {
-  console.log('0');
+'use strict';
+// Write a script that searches the second biggest integer in the list of arguments.
+const entrada = process.argv;
+const num_entrada = process.argv.length;
+
+if (!(entrada)) {
+  console.log(0);
+} else if (num_entrada < 4) {
+    console.log(0);
 } else {
-  value.sort(function (a, b) {
-    return a - b;
-  });
-  const valArr = value.slice(value.length - 2);
-  console.log(valArr[0]);
+    entrada.sort(function (a, b) {return a - b;});
+    const val_entrada = entrada.slice(num_entrada - 2);
+    console.log(val_entrada[0]);
 }
+
