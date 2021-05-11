f569a241d0c2de916a6d8f9dd3d336c0ebaae079
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 01:34:32 -0400

    hotfix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Write a script that prints 3 lines:  but by using an array of string and a loop
-const myArray = ['c is fun', 'Python is cool', 'JavaScript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i in myArray) {
   console.log(myArray[i]);
 }
