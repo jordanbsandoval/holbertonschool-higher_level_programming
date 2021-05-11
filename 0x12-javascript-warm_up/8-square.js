e738dc20c2743edff52c90749dcf35b0ffd5c872
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 11:33:42 -0400

    .repeat()

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
-if (isNaN(num)) {
+// write an script that print a square
+const entrada = process.argv[2];
+if (!entrada) {
   console.log('Missing size');
-} else {
-  for (let i = 0; i < num; i++) {
-    console.log('x'.repeat(num));
+} else if (isNaN(entrada)) {
+  console.log('Missing size');
+} else if (entrada > 0) {
+  for (let i = 0; i < entrada; i++) {
+    console.log('x'.repeat(entrada));
   }
 }
