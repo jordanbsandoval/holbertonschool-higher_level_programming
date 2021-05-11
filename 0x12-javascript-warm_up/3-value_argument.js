df0b2381b9128b162d51abf627ec7e27f19e1ca0
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 01:05:19 -0400

    operator!

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
-#!/usr/bin/node
-const descriptor = process.argv[2];
-if (descriptor !== undefined) {
-  console.log(descriptor);
-} else {
+#!/usr/bin/node                                                                                       
+// Write a script that prints the first argument passed to it
+const entrada = process.argv[2];
+if (!entrada) {
   console.log('No argument');
+} else if (entrada) {
+  console.log(entrada);
 }
