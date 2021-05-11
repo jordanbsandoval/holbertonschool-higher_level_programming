4f74775458512454c197271a0c608b851ff04fd4
jordanbsandoval <1608@holbertonschool.com>
2021-05-11 01:43:23 -0400

    hotfix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const number = process.argv[2];
-if (number === undefined) {
-  console.log('Missing number of ocurrences');
-} else if (number > 0) {
-  for (let i = 0; i < number; i++) {
+// Write a script that prints x times "C is fun"
+const entrada = process.argv[2];
+if (!entrada) {
+  console.log('Missing number of occurrences');
+} else if (entrada > 0) {
+  for (let i = 0; i < entrada; i++) {
     console.log('C is fun');
   }
 }
