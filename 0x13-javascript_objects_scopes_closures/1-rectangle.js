cf58952f3c384e977bbd0f1e4999750c254a6469
jordanbsandoval <1608@holbertonschool.com>
2021-06-09 17:37:35 -0400

    try2

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // exportando un objeto anonimo
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
-}
+};
